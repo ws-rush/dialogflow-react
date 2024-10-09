8fcfa63893e30b34e6aaeb7c874d28bcfb0ef20a
ws-rush <wusaby.rush@zohomail.com>
2024-10-09 03:11:46 +0300

    fix: refer types in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react(), dts({ tsconfigPath: './tsconfig.json' })],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: './lib/main.ts',
